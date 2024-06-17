17c1ec8785b9da8cbe61df2fcbcbc93076007235
pavithraj0809 <pavithraj0809@gmail.com>
2024-06-17 13:34:16 +0530

    package added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const app=express();
-const port=3001;
+
 const router=require('./router/router');
 
 
@@ -24,10 +24,8 @@ mongoose.connect('mongodb+srv://pavithra:pavi@cluster0.r1waizr.mongodb.net/one')
 router.get('/',(req,res)=>{
     res.send("Welcome to fruit back end");
 })
-.
 
 
 
-app.listen(port,()=>{
-    console.log("Sever Started");
-})
+
+module.exports=app;
